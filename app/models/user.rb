484adbf4dcd7a6fa3cc73984d02a293b7c8b669b
mayu1122oo <mayu.0310pcq123@gmail.com>
2023-03-17 20:15:05 +0900

    不要箇所の削除とテストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :email, presence: true
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
          validates :nickname, presence: true
-         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]/ }
          validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
