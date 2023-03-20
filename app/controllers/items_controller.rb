e804d8439ee846afdbaf1efd26cacea3b05310e0
mayu1122oo <mayu.0310pcq123@gmail.com>
2023-03-20 11:19:05 +0900

    商品出品機能のルーティングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,27 @@
 class ItemsController < ApplicationController
   
   def index
+    @items = Item.all
+  end
+
+  def new
+     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      @item = Item.new(item_params)
+      render.new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :item_state_id, :delivery_cost_id, :prefecture_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
 end
